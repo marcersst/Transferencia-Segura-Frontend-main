4e40a354904c9152a38b4058f69a4403032ba057
Marcelo <marcersstlg1@gmail.com>
2024-04-22 11:45:23 -0300

    correccion de pantalla blanca en mobil

--- src/context/contextApp.js
+++ src/context/contextApp.js
@@ -3,13 +3,11 @@ import { ethers } from 'ethers';
 import abi from "../helpers/abi.json";
 import abiusdt from "../helpers/abiusdt.json";
 import { useLocation, useNavigate } from 'react-router-dom';
-import Swal from 'sweetalert2'
-
+import Swal from 'sweetalert2';
 
 const AppContext = createContext();
 
 const ContratoProvider = ({ children }) => {
-    
     const [contrato, setContrato] = useState(null);
     const [contratousdt, setContratoUsdt] = useState(null);
     const [account, setAccount] = useState('Not connected');
@@ -20,11 +18,14 @@ const ContratoProvider = ({ children }) => {
     const esInicio = location.pathname === '/';
     const navigate = useNavigate();
 
+    const conectarContrato = async () => {
+        try {
+            const { ethereum } = window;
 
-    const conectarContrato = async () =>  {
-        const { ethereum } = window;
+            if (!ethereum) {
+                throw new Error('MetaMask no está instalado o no se detecta.');
+            }
 
-        if (ethereum) {
             const networkId = await ethereum.request({ method: 'eth_chainId' });
             const supportedNetwork = networkId === "0xaa36a7";
 
@@ -32,72 +33,67 @@ const ContratoProvider = ({ children }) => {
                 if (!supportedNetwork) {
                     Swal.fire("Por favor conectate a la red Sepolia!");
                 } else {
-                    try {
-                        const contractAddres = '0x5924A5Aa0B825754b83692ed894CfA35d494626D';
-                        const contractABI = abi;
+                    const contractAddres = '0x5924A5Aa0B825754b83692ed894CfA35d494626D';
+                    const contractABI = abi;
 
-                        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-                        const selectedAddress = accounts[0];
-                        setSigner(selectedAddress);
-                        if (ethereum) {
-                            ethereum.on('accountsChanged', (newAccounts) => {
-                                const newAddress = newAccounts[0];
-                                setSigner(newAddress);
-                            });
-                        }
+                    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+                    const selectedAddress = accounts[0];
+                    setSigner(selectedAddress);
 
-                        const provider = new ethers.providers.Web3Provider(ethereum);
-                        const signer = provider.getSigner();
-                        const contract = new ethers.Contract(contractAddres, contractABI, signer);
-
-                        setContrato({ provider, signer, contract });
-                    } catch (error) {
-                    }
-                }
-            } else {
-                if (signer && activo  !== 'crearTransferencias' && esInicio) {
-                    setActivo("Crear Transferencias");
-                    navigate('crearTransferencias');
-                }
-            }
-        } else {
-        }
-    };
-
-    const conectarContratoUSDT = async () => {
-        const { ethereum } = window;
-
-        if (ethereum) {
-            try {
-                const contractAddres = '0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0';
-                const contractABI = abiusdt;
-
-                const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-                const selectedAddress = accounts[0];
-                setSigner(selectedAddress);
-
-                if (ethereum) {
                     ethereum.on('accountsChanged', (newAccounts) => {
                         const newAddress = newAccounts[0];
                         setSigner(newAddress);
                     });
+
+                    const provider = new ethers.providers.Web3Provider(ethereum);
+                    const signer = provider.getSigner();
+                    const contract = new ethers.Contract(contractAddres, contractABI, signer);
+
+                    setContrato({ provider, signer, contract });
                 }
-
-                const provider = new ethers.providers.Web3Provider(ethereum);
-                const signer = provider.getSigner();
-                const usdt = new ethers.Contract(contractAddres, contractABI, signer);
-
-                setContratoUsdt({ provider, signer, usdt });
-
-            } catch (error) {
+            } else {
+                if (signer && activo !== 'crearTransferencias' && esInicio) {
+                    setActivo("Crear Transferencias");
+                    navigate('crearTransferencias');
+                }
             }
-        } else {
-            console.error('MetaMask no está instalado o no se detecta.');
+        } catch (error) {
+            console.error('Error al conectar con el contrato:', error);
+        }
+    };
+
+    const conectarContratoUSDT = async () => {
+        try {
+            const { ethereum } = window;
+
+            if (!ethereum) {
+                throw new Error('MetaMask no está instalado o no se detecta.');
+            }
+
+            const contractAddres = '0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0';
+            const contractABI = abiusdt;
+
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            const selectedAddress = accounts[0];
+            setSigner(selectedAddress);
+
+            ethereum.on('accountsChanged', (newAccounts) => {
+                const newAddress = newAccounts[0];
+                setSigner(newAddress);
+            });
+
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const usdt = new ethers.Contract(contractAddres, contractABI, signer);
+
+            setContratoUsdt({ provider, signer, usdt });
+        } catch (error) {
+            console.error('Error al conectar con el contrato USDT:', error);
         }
     };
 
     return (
-        <AppContext.Provider value={{contrato, contratousdt, conectarContrato, setAccount, account, signer, setSigner, idioma, setIdioma, activo, setActivo, esInicio,conectarContratoUSDT }}>
+        <AppContext.Provider value={{ contrato, contratousdt, conectarContrato, setAccount, account, signer, setSigner, idioma, setIdioma, activo, setActivo, esInicio, conectarContratoUSDT }}>
             {children}
         </AppContext.Provider>
     );
